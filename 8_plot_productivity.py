cf6ace2398de67125bb758b090c957017ee627b7
Thomas Goebel <40572777+tgoebel@users.noreply.github.com>
2019-10-10 15:06:09 -0500

    Add files via upload

--- 8_plot_productivity.py
+++ 8_plot_productivity.py
@@ -22,13 +22,10 @@ file_in    = 'hs_1981_2011_all.mat'
 
 dPar  = {
             'magRound'    : 1, # for binning
-            'a_Mc'         :  np.array([ 3.0, 4.0]),
+            'a_Mc'        :  np.array([ 3.0, 4.0]),
 
-            #--------------mag binning for prod. law--------------
-            'win' : .1, 'step' : .06,
-            'mag_exp' : 4.2, # exp. trendline goes through this point
             #=================plotting==============
-            'alpha'       : 1.04, # for plotting demonstration
+            'alpha'       : 1.0, # for plotting demonstration
             'plotFormat' : 'png',
             'xmin' :  2,  'xmax' : 8,
             'ymin' : 0.1, 'ymax' : 1e4,
@@ -49,22 +46,22 @@ for f_Mc in dPar['a_Mc']:
     #=================================2==========================================================================
     #                           count ave. no. of aftershocks per MS magnitude
     #============================================================================================================
-    aMag_bin = np.array( sorted(np.unique( np.around( m_N_as[0], dPar['magRound']))))
-    aAveNo_AS= np.ones( len( aMag_bin))*np.nan
-    aNo_Fam  = np.zeros( len( aMag_bin)) # total number of families within mag bin
-    aNo_AS20 = np.zeros( len( aMag_bin))
-    aNo_AS80 = np.zeros( len( aMag_bin))
+    aMag_round= np.around( m_N_as[0], dPar['magRound'])
+    aMag_bin  = np.array( sorted(np.unique( aMag_round)))
+    aAveNo_AS = np.ones( len( aMag_bin))*np.nan
+    aNo_Fam   = np.zeros( len( aMag_bin)) # total number of families within mag bin
+    aNo_AS20  = np.zeros( len( aMag_bin))
+    aNo_AS80  = np.zeros( len( aMag_bin))
 
     i = 0
     for curr_mag in aMag_bin:
-        selMag       = curr_mag == m_N_as[0]
+        selMag       = curr_mag == aMag_round
         aAveNo_AS[i] = m_N_as[1][selMag].mean()
-
         if selMag.sum() > 0:
             aNo_AS20[i]  = np.percentile( m_N_as[1][selMag], 20)
             aNo_AS80[i]  = np.percentile( m_N_as[1][selMag], 80)
         aNo_Fam[i]   = selMag.sum()
-        print curr_mag, 'sum', m_N_as[1][selMag].sum(), 'mean', aAveNo_AS[i],  aNo_AS20[i],aNo_AS80[i], 'no. of fam', aNo_Fam[i]
+        print curr_mag, 'mean N-AS', round(aAveNo_AS[i],2),  aNo_AS20[i],aNo_AS80[i], 'no. of fam', aNo_Fam[i]
 
         i += 1
 
@@ -74,14 +71,15 @@ for f_Mc in dPar['a_Mc']:
     plt.figure(1, figsize=(8,6))
     ax = plt.axes([.14,.12,.78,.83])#pPlot.createFigureSquare(1)
     ax.semilogy( m_N_as[0],   m_N_as[1],     'o',  ms = 6, mew =0, mfc = '.7', alpha = .2 )
-    ax.errorbar( aMag_bin,  aAveNo_AS, yerr=[np.zeros(aMag_bin.shape[0]), aNo_AS80-aAveNo_AS], 
-                 fmt = 'o', ecolor = 'k', elinewidth=.7,capsize=2.5, mec = 'k', ms = 8, mew = 1, mfc = 'w')
-    #ax.errorbar( aMag_bin,  aAveNo_AS, yerr=[aAveNo_AS-aNo_AS20, aNo_AS80-aAveNo_AS],
+    #ax.errorbar( aMag_bin,  aAveNo_AS, yerr=[np.zeros(aMag_bin.shape[0]), aNo_AS80-aAveNo_AS],
     #             fmt = 'o', ecolor = 'k', elinewidth=.7,capsize=2.5, mec = 'k', ms = 8, mew = 1, mfc = 'w')
+    ax.errorbar( aMag_bin,  aAveNo_AS, yerr=[aAveNo_AS-aNo_AS20, aNo_AS80-aAveNo_AS],
+                 fmt = 'o', ecolor = 'k', elinewidth=.7,capsize=2.5, mec = 'k', ms = 8, mew = 1, mfc = 'w')
 
     #-------------------------exponential - estimate-----------------------------------------------------
-    f_no_AS_pl = aAveNo_AS[aMag_bin == dPar['mag_exp']]
-    preFac     = np.log10( f_no_AS_pl) - dPar['alpha']*dPar['mag_exp']
+    mag_fit    = aMag_bin[10] # force fit through this point
+    f_no_AS_pl = aAveNo_AS[aMag_bin == mag_fit]
+    preFac     = np.log10( f_no_AS_pl) - dPar['alpha']*mag_fit
     a_N_hat    = 10**( dPar['alpha']*aMag_bin + preFac)
     ax.semilogy( aMag_bin, a_N_hat, 'w-')
     ax.semilogy( aMag_bin, a_N_hat, '-', color = 'r', lw = 2, label = 'exp = %.1f'%( np.round( dPar['alpha'],1)))
