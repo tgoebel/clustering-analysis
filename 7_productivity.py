ab07e2d63422302920430c714e233839012cbe93
Thomas Goebel <40572777+tgoebel@users.noreply.github.com>
2019-10-11 11:02:39 -0500

    Add files via upload

--- 7_productivity.py
+++ 7_productivity.py
@@ -76,7 +76,6 @@ for f_Mc in dPar['a_Mc']:
     a_mag_single = np.zeros( len( a_ID_single))
     a_N_AS_single= np.zeros( len( a_ID_single))
     a_N_FS_single= np.zeros( len( a_ID_single))
-
     for i in xrange( a_ID_single.shape[0]):
         # event ID may be in catalog more than once
         sel_ev          = eqCatMc.data['N'] == a_ID_single[i]
@@ -92,20 +91,20 @@ for f_Mc in dPar['a_Mc']:
     #=================================2==========================================================================
     #                   get MAGs of MS with aftershocks, count aftershocks
     #============================================================================================================
-    a_N_FS    = np.zeros( len( dClust.keys()))
-    a_N_AS    = np.zeros( len( dClust.keys()))
+    a_N_FS    = np.zeros( len( dClust.keys()), dtype = int)
+    a_N_AS    = np.zeros( len( dClust.keys()), dtype = int)
     a_MS_mag  = np.zeros( len( dClust.keys()))
+    a_MS_ID   = np.zeros( len( dClust.keys()), dtype = int)
     iCl = 0
     for sCl in dClust.keys():
-        aEqID = np.unique( dClust[sCl].flatten())
-        print 'cl: ', iCl+1,'out of: ', len( aEqID), aEqID
-        
+        aEqID = dClust[sCl]# np.unique( dClust[sCl].flatten()) unique is not needed anymore, createCluster has been fixed
+        print 'cl: ', iCl+1,'out of: ', len( dClust.keys()), 'no. of ev. in cl.', len( aEqID), len( np.unique( dClust[sCl]))
         # find MS mag and magnitude of entire family
         atmp_MAG = np.zeros( len( aEqID))
         atmp_Time= np.zeros( len( aEqID))
         a_iSel   = np.zeros( eqCatMc.size(), dtype = int)
+        # for each family find: event mag. and origin time
         for iM in xrange( len( aEqID)):
-            # event ID may be in catalog more than once
             sel_ev        = eqCatMc.data['N'] == aEqID[iM]
             if sel_ev.sum() != 1:
                 error_str = 'more/less than event found', eqCatMc.data['N'][sel_ev], aEqID[iM]
@@ -117,17 +116,21 @@ for f_Mc in dPar['a_Mc']:
         #catalog.selDicAll( np.logical_not(a_iSel))
         #----------------------------mainshock-------------------------------------------------- 
         selMS     = atmp_MAG == atmp_MAG.max()
-        tMS       = atmp_Time[selMS][0]
+        f_tMS     = atmp_Time[selMS][0]
+        i_ID_MS   = aEqID[selMS]
+
         #print 'tMS', tMS, v_currEqID.shape[0], 'MAG', curr_cat.data['MAG'][selMS][0]
         #----------------------------aftershock-------------------------------------------------- 
-        selAS     = atmp_Time > tMS
-        selFS     = atmp_Time < tMS
+        selAS     = atmp_Time > f_tMS
+        selFS     = atmp_Time < f_tMS
         #print 'no. of aftershocks', selAS.sum()
         # save number of aftershocks for each MS mag
-        a_MS_mag[iCl] =  atmp_MAG[selMS][0]#, dPar['magRound'])
+        a_MS_mag[iCl] = atmp_MAG[selMS][0]#, dPar['magRound'])
         a_N_AS[iCl]   = selAS.sum()
         a_N_FS[iCl]   = selFS.sum()
+        a_MS_ID[iCl]  = int( i_ID_MS[0])
         iCl += 1
+
     #=================================3==========================================================================
     #                  compare MS+single+FS+AS to original number of events in catalog
     #============================================================================================================
@@ -135,7 +138,7 @@ for f_Mc in dPar['a_Mc']:
     a_N_FS    = np.append( a_N_FS, a_N_FS_single)
     a_N_AS    = np.append( a_N_AS, a_N_AS_single)
     a_MS_mag  = np.append( a_MS_mag, a_mag_single)
-    
+    a_MS_ID   = np.append( a_MS_ID, a_ID_single)
     print 'tot ev. in catalog', n_aboveMc,'tot events in families',a_N_FS.sum() + a_N_AS.sum() + a_MS_mag.shape[0]
     #print 'N BG', a_mag_single.shape[0], 'FS', a_N_FS_single.sum(), 'AS', a_N_AS_single.sum(), 'MS (MS+BG)', a_MS_mag.shape[0]
 
@@ -143,8 +146,8 @@ for f_Mc in dPar['a_Mc']:
     #                    save to ASCII text
     #============================================================================================================
     file_out = '%s/%s_Nas_MS_Mc_%.1f.txt'%(data_dir, file_in.split('.')[0], f_Mc)#, dPar['magRound'])
-    np.savetxt( file_out, np.array([a_MS_mag, a_N_AS, a_N_FS]).T, fmt='%10.3f%10i%10i',
-                header = 'MAG          N-AS          N-FS; note N_AS=0 highlights singles or FS only')
+    np.savetxt( file_out, np.array([a_MS_mag, a_N_AS, a_N_FS, a_MS_ID]).T, fmt='%10.3f%10i%10i%14i',
+                header = 'MAG          N-AS          N-FS        MS-ID; note N_AS=0 highlights singles or FS only')
     iMc += 1
 
 
