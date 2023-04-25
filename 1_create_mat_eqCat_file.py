def610326bd49ee4f7af40d792e4df2995eaca02
Thomas Goebel <40572777+tgoebel@users.noreply.github.com>
2023-04-25 07:35:39 -0500

    Add files via upload

--- 1_create_mat_eqCat_file.py
+++ 1_create_mat_eqCat_file.py
@@ -5,19 +5,15 @@ Created on March 28th,  2019
 - load Hauksson, Shearer 2011 eq catalog from scec data center, alterntive catalogs
 - save as .mat binary for fast data I/O
 - note that the original catalog is not provided and has to be downloaded from the web:
-  https://scedc.caltech.edu/research-tools/altcatalogs.html
+  https://scedc.caltech.edu/eq-catalogs/altcatalogs.html
 
 @author: tgoebel
 '''
 #------------------------------------------------------------------------------
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.io
 import os
 
-
 #------------------------------my modules-------------------------------------- 
-from src.EqCat import *
+from src.EqCat import EqCat
 
 eqCat = EqCat( )
 
@@ -26,14 +22,18 @@ eqCat = EqCat( )
 #================================================================================
 # change to local dir where eq. catalogs are saved
 # the original catalog can be found here: https://scedc.caltech.edu/research-tools/altcatalogs.html
-dir_in = '%s/data/quakeData/SCSN/relocated'%( os.path.expanduser('~'))
+dir_in = 'data'
 file_in= 'hs_1981_2011_all.txt'
 
 #=================================2==============================================
 #                            load data
 #================================================================================
-os.chdir( dir_in)
-eqCat.loadEqCat(  file_in, 'HS_reloc', removeColumn=[24,25,26])
+import numpy as np
+# 0-5 (datetime), 6(ID), 7 (lat), 8 (lon), 9 (depth), 10 (mag)
+mData = np.loadtxt( f"{dir_in}/{file_in}", usecols=(0,1,2,3,4,5,6,7,8,9, 10)).T
+print( mData.shape)
+
+eqCat.loadEqCat( f"{dir_in}/{file_in}", 'HS_reloc')
 
 print( 'total no. of events: ', eqCat.size())
 print( sorted( eqCat.data.keys()))
